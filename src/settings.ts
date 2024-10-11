161bf13d72a627a1b87ea18faf693cdb9cfe599c
KyanBirb <littlekbirb@gmail.com>
2024-10-10 21:49:21 -0500

    Add a settings menu

---
+++ src/settings.ts
@@ -0,0 +1,60 @@
+import { App, PluginSettingTab, Setting } from "obsidian";
+import HexPatternMarkdown from "./main";
+
+export interface HexPatternMarkdownSettings {
+	patternColorStart: string;
+    patternColorEnd: string;
+    animatePattern: boolean;
+}
+
+export const DEFAULT_SETTINGS: Partial<HexPatternMarkdownSettings> = {
+    patternColorStart: "#7f6df2",
+    patternColorEnd: "#423975",
+    animatePattern: true
+};
+
+export class HexPatternMarkdownSettingsTab extends PluginSettingTab {
+
+    plugin: HexPatternMarkdown;
+
+    constructor(app: App, plugin: HexPatternMarkdown) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display(): void {
+        const containerElement = this.containerEl;
+
+        containerElement.empty();
+
+        new Setting(containerElement)
+            .setName('Pattern start color')
+            .addColorPicker(colorPicker => colorPicker
+                .setValue(this.plugin.settings.patternColorStart)
+                .onChange(async color => {
+                    this.plugin.settings.patternColorStart = color;
+                    await this.plugin.saveSettings();
+                })
+            );
+        
+        new Setting(containerElement)
+            .setName('Pattern end color')
+            .addColorPicker(colorPicker => colorPicker
+                .setValue(this.plugin.settings.patternColorEnd)
+                .onChange(async color => {
+                    this.plugin.settings.patternColorEnd = color;
+                    await this.plugin.saveSettings();
+                })
+            );
+
+        new Setting(containerElement)
+            .setName('Animate patterns')
+            .addToggle(toggle => toggle
+                .setValue(this.plugin.settings.animatePattern)
+                .onChange(async color => {
+                    this.plugin.settings.animatePattern = color;
+                    await this.plugin.saveSettings();
+                })
+            );
+    }
+}
